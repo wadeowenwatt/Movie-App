aff842f7ac92c241423a7e34852c5df2c9ba06ff
Trần Nhật Linh <trannhatlinh120801@gmail.com>
2022-08-05 16:49:25 +0700

    fix splash screen have white screen

--- app/src/main/java/com/example/movieapp/presentation/splash/SplashActivity.kt
+++ app/src/main/java/com/example/movieapp/presentation/splash/SplashActivity.kt
@@ -4,18 +4,14 @@ import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.CountDownTimer
 import android.os.Handler
 import android.view.View
 import android.view.WindowManager
-import androidx.activity.viewModels
 import com.example.movieapp.R
 import com.example.movieapp.databinding.ActivitySplashBinding
-import com.example.movieapp.presentation.home.HomeViewModel
 import com.example.movieapp.presentation.main.MainActivity
 
 class SplashActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
